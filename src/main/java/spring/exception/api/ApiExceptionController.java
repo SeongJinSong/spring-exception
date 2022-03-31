2eb324c5e8954bbbaf87dd1d603a55a91faa04ff
seongjin_song <seongjin_song@tmax.co.kr>
2022-03-31 18:00:09 +0900

    DefaultHandlerExceptionResolver

--- src/main/java/spring/exception/api/ApiExceptionController.java
+++ src/main/java/spring/exception/api/ApiExceptionController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import spring.exception.exception.BadRequestException;
@@ -30,6 +31,11 @@ public class ApiExceptionController {
         }
 
         return new MemberDto(id, "hello" + id);
+    }
+
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
     }
 
     @GetMapping("/api/response-status-ex1")
